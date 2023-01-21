6179d1107f3e48d1f6fac68074068241ff2b4042
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2023-01-21 06:48:47 -0300

    Add: homepage

--- src/components/Habit.tsx
+++
@@ -1,9 +0,0 @@
-interface HabitProps {
-  completed: number
-}
-
-export function Habit(props : HabitProps) {
-  return (
-    <div className="bg-zinc-900 w-10 h-10 text-white text-center rounded m-2 flex items-center justify-center">{ props.completed }</div>
-  )
-}
